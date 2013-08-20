370fb17e53e1387f6ce5e5e15ee4a5fdc8a34e1c
Sebastián Katzer <katzer.sebastian@googlemail.com>
2013-08-20 18:27:33 +0200

    Initial start of Android support

---
+++ src/android/Printer.java
@@ -0,0 +1,28 @@
+/**
+ *  Printer.java
+ *  Cordova Printing Plugin
+ *
+ *  Created by Sebastian Katzer (github.com/katzer) on 20/08/2013.
+ *  Copyright 2013 Sebastian Katzer. All rights reserved.
+ *  GPL v2 licensed
+ */
+
+package de.appplant.cordova.plugin;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.text.Html;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+public class Printer extends CordovaPlugin {
+
+}
