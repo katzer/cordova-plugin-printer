b417de709364e7d45a8d847a2420908446ef8865
Sebastián Katzer <katzer.sebastian@googlemail.com>
2014-09-12 16:45:05 +0200

    added CordovaLib stuff to gitignore

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/sebastian/Documents/github/cordova-plugin-printer/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/sebastian/Documents/github/cordova-plugin-printer/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/sebastian/Documents/github/cordova-plugin-printer/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
