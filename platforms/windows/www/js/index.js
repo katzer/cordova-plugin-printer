4773a2cc53d44de688598864dac2586761e17193
Sebastián Katzer <katzer.sebastian@googlemail.com>
2016-08-02 19:12:54 +0200

    Update project

--- platforms/windows/www/js/index.js
+++ platforms/windows/www/js/index.js
@@ -73,13 +73,13 @@ var app = {
         page += '<body><div class="promos"><div class="promo"><div class="deal"><span>Premium</span><span>This is really a good deal!</span></div><span class="price">$79</span><ul class="features"><li>Some great feature</li><li>Another super feature</li><li>And more...</li></ul><button>Sign up</button></div><div class="promo scale"><div class="deal"><span>Plus</span><span>This is really a good deal!</span></div><span class="price">$89</span><ul class="features"><li>Some great feature</li><li>Another super feature</li><li>And more...</li></ul><button>Sign up</button></div><div class="promo"><div class="deal"><span>Basic</span><span>Basic membership</span></div><span class="price">$69</span><ul class="features"><li>Choose the one on the left</li><li>We need moneyy</li><li>And more...</li></ul><button>Sign up</button></div></div></body>';
 
         cordova.plugins.printer.print(page, options, function (res) {
-            alert(res ? 'Sent' : 'Canceled');
+            alert(res ? 'Done' : 'Canceled');
         });
     }
 };
 
 if (window.hasOwnProperty('Windows')) {
-    alert = function (msg) { new Windows.UI.Popups.MessageDialog(msg).showAsync(); }
+    alert = function (msg) { new Windows.UI.Popups.MessageDialog(msg).showAsync(); };
 }
 
 app.initialize();
