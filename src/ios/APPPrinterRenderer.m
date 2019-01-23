e1405e914b41da4c13bc7754edb2221a37e72f68
Sebastián Katzer <katzer.sebastian@googlemail.com>
2019-01-23 15:45:40 +0100

    Cleanup APPPrinterRenderer by introducing APPPrinterStyle

--- src/ios/APPPrinterRenderer.m
+++ src/ios/APPPrinterRenderer.m
@@ -21,6 +21,7 @@
 
 #include "APPPrinterLayout.h"
 #include "APPPrinterRenderer.h"
+#include "APPPrinterStyle.h"
 #include "APPPrinterUnit.h"
 
 @interface APPPrinterRenderer ()
@@ -117,7 +118,6 @@
                           inRect:(CGRect)rect
 {
     NSString *label = spec[@"text"];
-    NSString *align = spec[@"align"];
     BOOL showIndex  = [spec[@"showPageIndex"] boolValue];
 
     if (showIndex)
@@ -129,103 +129,15 @@
     if ([self strIsNullOrEmpty:label])
         return;
 
-    NSMutableParagraphStyle *style = [[NSParagraphStyle defaultParagraphStyle]
-                                      mutableCopy];
+    APPPrinterStyle *style = [[APPPrinterStyle alloc]
+                              initWithDictionary:spec];
 
-    if ([align isEqualToString:@"left"])
-    {
-        style.alignment = NSTextAlignmentLeft;
-    }
-    else if ([align isEqualToString:@"right"])
-    {
-        style.alignment = NSTextAlignmentRight;
-    }
-    else if ([align isEqualToString:@"center"])
-    {
-        style.alignment = NSTextAlignmentCenter;
-    }
-    else if ([align isEqualToString:@"justified"])
-    {
-        style.alignment = NSTextAlignmentJustified;
-    }
-
-    NSString *hex  = spec[@"color"];
-    UIColor *color;
-
-    if ([self strIsNullOrEmpty:hex])
-    {
-        color = UIColor.darkTextColor;
-    }
-    else
-    {
-        if ([hex characterAtIndex:0] == '#')
-        {
-            hex = [hex substringFromIndex:1];
-        }
-
-        color = [self colorFromHexString:hex];
-    }
-
-    if (!color)
-    {
-        color = UIColor.darkTextColor;
-    }
-
-    double size = [spec[@"size"] doubleValue];
-
-    if (size <= 0.0)
-    {
-        size = UIFont.smallSystemFontSize;
-    }
-
-    UIFont *font = [UIFont fontWithName:spec[@"font"] size:size];
-
-    if (!font)
-    {
-        font = [UIFont systemFontOfSize:size];
-    }
-
-    UIFontDescriptorSymbolicTraits traits = 0;
-
-    if ([spec[@"bold"] boolValue])
-    {
-        traits = traits | UIFontDescriptorTraitBold;
-    }
-
-    if ([spec[@"italic"] boolValue])
-    {
-        traits = traits | UIFontDescriptorTraitItalic;
-    }
-
-    font = [UIFont fontWithDescriptor:[font.fontDescriptor fontDescriptorWithSymbolicTraits:traits]
-                                 size:size];
-
-    NSDictionary *attrs = @{ NSFontAttributeName: font,
-                             NSParagraphStyleAttributeName: style,
-                             NSForegroundColorAttributeName: color };
-
-    [label drawInRect:rect withAttributes:attrs];
+    [label drawInRect:rect withAttributes:style.attributes];
 }
 
 - (BOOL) strIsNullOrEmpty:(NSString *)string
 {
     return [string isEqual:[NSNull null]] || string.length == 0;
-}
-
-- (UIColor *)colorFromHexString:(NSString *)hex
-{
-    unsigned rgb = 0;
-
-    NSScanner *scanner = [NSScanner scannerWithString:hex];
-
-    [scanner setCharactersToBeSkipped:
-     [NSCharacterSet characterSetWithCharactersInString:@"#"]];
-
-    [scanner scanHexInt:&rgb];
-
-    return [UIColor colorWithRed:((rgb & 0xFF0000) >> 16)/255.0
-                           green:((rgb & 0xFF00) >> 8)/255.0
-                            blue:(rgb & 0xFF)/255.0 alpha:1.0];
 }
 
 @end
